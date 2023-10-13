adeb0e333de42ad337e82f14f8de15aa6ae589ae
Cosmikyu <samuel.steingrimsson@gmail.com>
2023-10-13 17:14:36 +0000

    Initial commit

---
+++ src/main/java/net/cosmikyu/gettingit/GettingIntoItDataGenerator.java
@@ -0,0 +1,11 @@
+package net.cosmikyu.gettingit;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GettingIntoItDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
